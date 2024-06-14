47b20f1fb0d1ecdf9a542642d211f6ce28c59bc2
Aditya Panchal <adipanchal172@gmail.com>
2024-06-14 14:40:50 +0530

    Todo List

---
+++ Day-2- Todo List/test/src/app/Todo.ts
@@ -0,0 +1,6 @@
+export class Todo {
+  sno: number = 0; // Default value for sno
+  title: string = ""; // Default empty string for title
+  desc: string = ""; // Default empty string for desc
+  active: boolean = true; // Default active state
+}
